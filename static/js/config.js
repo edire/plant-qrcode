c468c9a317a8ed1e205b5ad84824c790ba6ae007
edire <az8641683@163.com>
2015-10-27 10:00:52 +0800

    auto root

--- static/js/config.js
+++ static/js/config.js
@@ -2,7 +2,7 @@
 var app = {};
 
 var root = location.origin;
-if (root.indexOf('127.0.0.1')) {
+if (root.indexOf('127.0.0.1') > -1) {
     app.root = 'http://127.0.0.1/qrcode';
 } else {
     app.root = root;
