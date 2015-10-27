bd8376b5094ec30ed98eaa05b682f6fc9cd678e6
edire <az8641683@163.com>
2015-10-27 09:57:38 +0800

    auto root

--- static/js/config.js
+++ static/js/config.js
@@ -1,4 +1,11 @@
 
 var app = {};
-app.root = 'http://127.0.0.1/qrcode';
+
+var root = location.origin;
+if (root.indexOf('127.0.0.1')) {
+    app.root = 'http://127.0.0.1/qrcode';
+} else {
+    app.root = root;
+}
+
 app.qiniu = 'http://7xnsf1.com1.z0.glb.clouddn.com/';
